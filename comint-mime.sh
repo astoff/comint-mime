1fc568022927477ef268df53d4587ab6872917f8
Augusto Stoffel <arstoffel@gmail.com>
2022-08-14 12:29:25 +0200

    Make comint-mime.sh more portable
    Closes #3.
    Closes #9.

--- comint-mime.sh
+++ comint-mime.sh
@@ -6,7 +6,7 @@ mimecat () {
     local file
     case "$1" in
         -h|--help)
-            echo "Usage: mimecat [-t TYPE] [FILE]"
+            echo >&2 "Usage: mimecat [-t TYPE] [FILE]"
             return 0
             ;;
         -t|--type)
@@ -16,14 +16,14 @@ mimecat () {
     esac
     if [ -z "$1" ]; then
         if [ -z "$type" ]; then
-           echo "mimecat: When reading from stdin, please provide -t TYPE"
+           echo >&2 "mimecat: When reading from stdin, please provide -t TYPE"
            return 1
         fi
         base64 | xargs -0 printf '\033]5151;{"type":"%s"}\n%s\033\\\n' "$type"
     else
         file=$(realpath -e "$1") || return 1
-        [ -n "$type" ] || type=$(file -bi "$file")
+        [ -n "$type" ] || type=$(file -b --mime "$file")
         printf '\033]5151;{"type":"%s"}\nfile://%s%s\033\\\n' \
-               "$type" "$(hostname)" "$file"
+               "$type" "$(uname -n)" "$file"
     fi
 }
