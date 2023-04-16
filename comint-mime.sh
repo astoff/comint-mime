dfc5ce93e1e69408dbd1dabc191bd02fe0333c69
Augusto Stoffel <arstoffel@gmail.com>
2023-04-16 10:43:50 +0200

    Refactor shell script

--- comint-mime.sh
+++ comint-mime.sh
@@ -1,20 +1,18 @@
 # This file is part of https://github.com/astoff/comint-mime
-# shellcheck shell=sh
+# shellcheck shell=dash
 
 mimecat () {
-    local type
-    local file
+    local type file
     case "$1" in
         -h|--help)
-            echo >&2 "Usage: mimecat [-t TYPE] [FILE]"
+            echo >&2 "\
+Usage: mimecat [-t TYPE] [FILE]
+Display contents of FILE as mime TYPE."
             return 0
             ;;
-        -t|--type)
-            type="$2"
-            shift; shift
-            ;;
+        -t|--type) type="$2"; shift 2;;
     esac
-    if [ -z "$1" ]; then
+    if [ $# -eq 0 ]; then
         if [ -z "$type" ]; then
            echo >&2 "mimecat: When reading from stdin, please provide -t TYPE"
            return 1
@@ -23,7 +21,6 @@ mimecat () {
     else
         file=$(realpath -e "$1") || return 1
         [ -n "$type" ] || type=$(file -b --mime "$file")
-        printf '\033]5151;{"type":"%s"}\nfile://%s%s\033\\\n' \
-               "$type" "$(uname -n)" "$file"
+        printf '\033]5151;{"type":"%s"}\nfile://%s%s\033\\\n' "$type" "$(uname -n)" "$file"
     fi
 }
